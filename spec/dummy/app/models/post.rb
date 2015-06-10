6a17c97316a2914a4cccd5800f2cde0f6c875902
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-06-10 19:44:47 -0300

    fix for Rails 4

--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :author, :category if Rails::VERSION::MAJOR == 3
 
-  scope :even, where('id % 2 = 0')
-  scope :odd, where('id % 2 = 1')
+  scope :even, -> { where('id % 2 = 0') }
+  scope :odd, -> { where('id % 2 = 1') }
   scope :greater_than, lambda { |gt_id| where('id > ?', gt_id) }
 end
